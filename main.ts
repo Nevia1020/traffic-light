457769e9afcb30ae714e041c63b542843069d7f2
Nevia1020 <79929396+Nevia1020@users.noreply.github.com>
2021-03-24 13:12:45 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -9,9 +9,9 @@ input.onButtonPressed(Button.A, function () {
         basic.pause(2000)
         pins.digitalWritePin(DigitalPin.P0, 1)
         pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(500)
+        basic.pause(5000)
         pins.digitalWritePin(DigitalPin.P2, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P0, 0)
     }
 })
 // pin0=red
