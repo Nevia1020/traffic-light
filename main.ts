3bb7a2bb8f81f4a4cc185afec29eb1485fe4fbf3
Nevia1020 <79929396+Nevia1020@users.noreply.github.com>
2021-03-24 13:03:16 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,18 @@
 // pin0=red
 // pin1=green
 input.onButtonPressed(Button.A, function () {
-    pins.digitalWritePin(DigitalPin.P2, 1)
-    basic.pause(5000)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    pins.digitalWritePin(DigitalPin.P1, 1)
-    basic.pause(2000)
-    pins.digitalWritePin(DigitalPin.P0, 1)
-    pins.digitalWritePin(DigitalPin.P1, 0)
+    while (true) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(5000)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+        basic.pause(2000)
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+        basic.pause(500)
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
 })
 // pin0=red
 // pin1=green
@@ -20,6 +25,8 @@ input.onButtonPressed(Button.B, function () {
     pins.digitalWritePin(DigitalPin.P0, 0)
     basic.pause(100)
     pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P2, 0)
 })
 basic.forever(function () {
 	
